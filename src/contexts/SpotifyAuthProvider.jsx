1c499e85c95b9fcaff208a4f452078f455831b7b
Alex <alex.holder@bigfootds.com>
2024-10-05 11:54:58 +1000

    fix: correct callback url in deployment

--- src/contexts/SpotifyAuthProvider.jsx
+++ src/contexts/SpotifyAuthProvider.jsx
@@ -68,7 +68,7 @@ export function SpotifyAuthProvider({children}){
 		params.append("grant_type", "authorization_code");
 		params.append("code", code);
 		// params.append("redirect_uri", process.env.SOME_SPOTIFY_REDIRECT_URI);
-		params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+		params.append("redirect_uri", import.meta.env.VITE_SPOTIFY_CALLBACK);
 		params.append("code_verifier", verifier);
 
 		// https://api.spotify.com/auth?client_id=oqeihrt90183yrt2ogknjs&code=
@@ -95,7 +95,7 @@ export function SpotifyAuthProvider({children}){
 		const params = new URLSearchParams();
 		params.append("client_id", clientId);
 		params.append("response_type", "code");
-		params.append("redirect_uri", "http://localhost:5173/spotifycallback");
+		params.append("redirect_uri", import.meta.env.VITE_SPOTIFY_CALLBACK);
 		params.append("scope", "user-top-read user-read-private user-read-email");
 		params.append("code_challenge_method", "S256");
 		params.append("code_challenge", challenge);
